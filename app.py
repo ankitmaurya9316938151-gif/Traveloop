8654e370050937a0b85d89e5ba48d426cf09a731
Your Name <your-email@example.com>
2026-05-10 10:19:42 +0530

    Added basic trip creation and display table

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
+
 
 trips = []
 
@@ -10,13 +11,21 @@ def index():
 
 @app.route('/add_trip', methods=['POST'])
 def add_trip():
-   
+
     destination = request.form.get('destination')
-    budget = request.form.get('budget')
+    budget = int(request.form.get('budget'))
     
+
+    new_trip = {
+        'id': len(trips) + 1,
+        'destination': destination,
+        'budget': budget,
+        'spent': 0
+    }
+    trips.append(new_trip)
     
-    trips.append({'destination': destination, 'budget': budget})
-    return redirect(url_for('index'))
+   
+    return redirect('/')
 
 if __name__ == '__main__':
     app.run(debug=True, port=5001)
